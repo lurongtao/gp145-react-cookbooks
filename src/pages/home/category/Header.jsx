7c9b626719386d414d65a067465f75b359ae4225
lurongtao <lurongtao@126.com>
2019-12-18 18:02:29 +0800

    2019-12-18-pm

--- src/pages/home/category/Header.jsx
+++ src/pages/home/category/Header.jsx
@@ -47,19 +47,19 @@ import { withRouter } from 'react-router-dom'
 const Header = (props) => {
   let getPath = useCallback(() => {
     return props.location.pathname === '/' ? 'category' : props.location.pathname.substr(1)
-  })
+  }, [])
 
   let [dir, setDir] = useState(getPath(props))
 
-  let handleClick = useMemo((dir) => {
-    console.log(dir)
-    return () => {
-      // this.setState({
-      //   dir
-      // })
-      // this.props.history.push(`/${dir}`)
-    }
-  })
+  let handleClick = useCallback(
+    (dir) => {
+      return () => {
+        setDir(dir)
+        props.history.push(`/${dir}`)
+      }
+    },
+    []
+  )
 
   return (
     <HeaderWrap>
@@ -70,9 +70,13 @@ const Header = (props) => {
       >
         <li
           onClick={handleClick('category')}
+          className={dir === 'category' ? 'active' : ''}
         >分类</li>
         <li
+          onClick={handleClick('material')}
+          className={dir === 'material' ? 'active' : ''}
         >食材</li>
+        <li className={`slider ${dir === 'material' ? 'right' : ''}`}></li>
       </SliderArea>
     </HeaderWrap>
   )
